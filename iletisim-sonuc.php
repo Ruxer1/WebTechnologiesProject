a638d02a0e11a517d6fffb807c12fe345eee496c
Ruxer1 <ofarukkaymas233@gmail.com>
2025-05-25 17:51:48 +0300

    Baş harfim ile oluşturulan favicon tüm sayfalara eklendi.

--- iletisim-sonuc.php
+++ iletisim-sonuc.php
@@ -5,6 +5,7 @@
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>İletişim Sonucu</title>
+  <link rel="icon" href="images/favicon.ico" type="image/x-icon">
   <link rel="stylesheet" href="style.css" />
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" />
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
@@ -23,7 +24,7 @@
         <li><a href="login.php"><i class="fa-solid fa-right-to-bracket"></i> Giriş</a></li>
       </ul>
     </nav> <br>
-    
+
     <h1>İletişim Formu Gönderildi</h1>
   </header>
 
